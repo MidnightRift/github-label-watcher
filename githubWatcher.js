2e857491fda09ad6a44b57e15989025af9643685
Midnight <midnight@midnightrift.com>
2017-08-03 00:29:42 -0700

    Initial commit

---
+++ githubWatcher.js
@@ -0,0 +1,70 @@
+let GitHubApi = require('github'),
+    axios = require('axios'),
+    ff = require('./flatfile.js');
+
+let github = new GitHubApi({
+    debug: true,
+
+    protocol: 'https',
+    headers: {
+        'user-agent': 'github-label-watcher'
+    },
+    Promise: require('bluebird'),
+    followRedirects: false,
+    timeout: 5000
+});
+
+
+github.issues.getForRepo({
+        owner: '<INSERRT REPO OWNER>',
+        repo: '<INSERT REPO',
+        state: 'open',
+        labels: '<INSERT LABELs>'
+    }
+).then(function (res) {
+
+    if (res.data < 1) return Promise.reject('No PR');
+
+    let wantedData = {
+        html_url: res.data[0].html_url,
+        id: res.data[0].id,
+        number: res.data[0].number,
+        title: res.data[0].title,
+        created_at: res.data[0].created_at,
+        body: res.data[0].body
+    };
+
+    ff.get(wantedData.id).then(function (res) {
+        if (!res) {
+
+            let msg = {
+                text: '<!everyone|everyone>',
+                attachments: [
+                    {
+                        'fallback': 'New master -> stable',
+                        'pretext': '*New master -> stable*',
+                        'title': 'PR: ' + wantedData.number + ' - ' + wantedData.title,
+                        'title_link': wantedData.html_url,
+                        'text': wantedData.body,
+                        'color': '#7CD197',
+                        'mrkdwn_in': ['title', 'pretext']
+                    }
+                ]
+            };
+
+            let calls = [
+                axios.post('<INSERT URL>', msg
+                ),
+                axios.post('<INSERT URL>', msg
+                )];
+
+            axios.all(calls).then(function (res) {
+                ff.save(wantedData.id)
+            })
+        }
+    })
+}).catch(function (err) {
+    if (!err === 'No PR') {
+        console.log(err);
+    }
+});
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Gihub Label Watcher
+
+- This is a label watcher for github, its designed to send messages to slack. Change the fields in github watcher that are like <INSERT something here.>
+- in the axios calls you can have as many calls as you want to your differnt slack channels just insert the required URL.
+- setup the script to run in a cron I every 5 or 10 min you have a rate limit of 60 reqs / Hour unless you authenticate the app
